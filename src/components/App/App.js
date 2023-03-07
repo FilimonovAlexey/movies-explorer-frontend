f83324e8f55ce3cd8507eed1b4ed3c4bc06b0468
Алексей Филимонов <alexeyf08@gmail.com>
2023-03-08 03:16:58 +0700

    сверстан и добавлен роут на страницу 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import '../App/App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../Main/Main"
+import Page404 from '../Page404/Page404';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <div className='App'>
         <Routes>
           <Route exact path="/" element={<Main/>} />
+          
+          <Route exact path="*" element={<Page404/>} />
         </Routes>
       </div>
     </BrowserRouter>
