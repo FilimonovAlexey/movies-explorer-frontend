bb73219abd33232f9c033a39ce848460d6b3a79c
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-09 20:22:50 +0700

    сверстан блок login + добавлен роут

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../Main/Main"
 import Page404 from '../Page404/Page404';
 import Movies from '../Movies/Movies';
+import Login from '../Login/Login';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Main/>} />
           <Route exact path="/Movies" element={<Movies/>} />
-
+          <Route exact path="/signin" element={<Login/>} />
           <Route exact path="*" element={<Page404/>} />
         </Routes>
       </div>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Login.css";
+import logo from "../../images/logo.svg";
+import { Link } from "react-router-dom";
+
+function Login() {
+  return(
+    <section className="login">
+      <div className="login__box">
+
+            <div className="login__logo-box">
+              <Link to="/">
+                <img src={logo} alt="Логотип" className="login__logo" />
+              </Link>
+            </div>
+        <h2 className="login__title">Рады видеть!</h2>
+        <form className="login__form" name="login-form">
+          <div className="login__field">
+              <label>
+                  <span className="login__email">E-mail</span>
+                  <input className="login__input" 
+                         type="email" 
+                         name="email" 
+                         placeholder="Введите Ваш E-mail" 
+                         required 
+                  />
+              </label>
+              <div className="logo__line"></div>
+              <label>
+                  <span className="login__password">Пароль</span>
+                  <input className="login__input" 
+                         type="password" 
+                         name="password" 
+                         placeholder="Введите Ваш Пароль" 
+                         minLength="4" 
+                         required 
+                   />
+              </label>
+              <div className="logo__line"></div>
+          </div>
+          <div className="login__button-box">
+              <button className="login__button" type="submit">Войти</button>
+              <Link className="login__link" to="/signup">
+                  Ещё не зарегистрированы?
+                  <span className="login__register">Регистрация</span>
+              </Link>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
