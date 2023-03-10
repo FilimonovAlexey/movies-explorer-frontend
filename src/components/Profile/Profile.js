989c223143b089403aa1d77e63f7373a99be1b0b
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-11 01:41:16 +0700

    сверстан и оформлен блок profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,35 +7,38 @@ function Profile() {
   return(
     <>
     <Header loged={true}/>
-    <div className='profile__content'>
-      <h2 className='profile__title'>Привет, Виталий!</h2>
-      <form className='profile__form'>
-          <fieldset className='profile__fieldset'>
-              <label className='profile__fields'>
-                  <p className='profile__input-name'>Имя</p>
-                  <input className='profile__input'
-                      type='text'
-                      name='name'
-                      placeholder='Имя'
-                      defaultValue={'Виталий'}
-                      required />
-              </label>
-              <label className='profile__fields'>
-                  <p className='profile__input-edit'>E-mail</p>
-                  <input className='profile__input'
-                      type='email'
-                      name='name'
-                      defaultValue={'pochta@yandex.ru'}
-                      placeholder='E-mail'
-                      required />
-              </label>
-          </fieldset>
-          <div className='profile__nav'>
-              <button className='profile__button profile__button_edit' type='submit'>Редактировать</button>
-              <Link className='profile__button profile__button_signin' to='/signin'>Выйти из аккаунта</Link>
-          </div>
-      </form>
-    </div>
+    <section className="profile">
+      <div className='profile__content'>
+        <h2 className='profile__title'>Привет, Виталий!</h2>
+        <form className='profile__form'>
+            <fieldset className='profile__fieldset'>
+                <label className='profile__fields'>
+                    <p className='profile__input-name'>Имя</p>
+                    <input className='profile__input'
+                        type='text'
+                        name='name'
+                        placeholder='Имя'
+                        defaultValue={'Виталий'}
+                        required />
+                </label>
+                <div className="profile__line"></div>
+                <label className='profile__fields'>
+                    <p className='profile__input-email'>E-mail</p>
+                    <input className='profile__input'
+                        type='email'
+                        name='name'
+                        defaultValue={'pochta@yandex.ru'}
+                        placeholder='E-mail'
+                        required />
+                </label>
+            </fieldset>
+            <div className='profile__nav'>
+                <button className='profile__button_edit' type='submit'>Редактировать</button>
+                <Link className='profile__button_signin' to='/signin'>Выйти из аккаунта</Link>
+            </div>
+        </form>
+      </div>
+    </section>
     </>
   );
 }
