07e1b547e2567674d0150e733279629508dfd4ba
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-11 01:06:44 +0700

    fix: исправлено отоброжение блока techs на на мобильных устройствах

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -3,23 +3,23 @@ import "./Techs.css";
 
 function Techs() {
   return (
-    <section className="techs__container">
-      <div className="techs__title_container">
+    <section className="techs">
+      <div className="techs__box">
         <h2 className="techs__title">Технологии</h2>
+        <h3 className="techs__subtitle">7 технологий</h3>
+        <p className="techs__about">На курсе веб-разработки мы освоили технологии, которые применили в дипломном 
+            проекте.
+        </p>
+        <ul className="techs__cards">
+          <li className="techs__card">HTML</li>
+          <li className="techs__card">CSS</li>
+          <li className="techs__card">JS</li>
+          <li className="techs__card">React</li>
+          <li className="techs__card">Git</li>
+          <li className="techs__card">Express.js</li>
+          <li className="techs__card">mongoDB</li>
+        </ul>
       </div>
-      <h3 className="techs__subtitle">7 технологий</h3>
-      <p className="techs__about">На курсе веб-разработки мы освоили технологии, которые применили в дипломном 
-          проекте.
-      </p>
-      <ul className="techs__cards">
-        <li className="techs__card">HTML</li>
-        <li className="techs__card">CSS</li>
-        <li className="techs__card">JS</li>
-        <li className="techs__card">React</li>
-        <li className="techs__card">Git</li>
-        <li className="techs__card">Express.js</li>
-        <li className="techs__card">mongoDB</li>
-      </ul>
     </section>
   );
 }
