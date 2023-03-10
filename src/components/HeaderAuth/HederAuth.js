231564db05a04ca795d4bf17b6f56c1634270a83
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-10 22:47:37 +0700

    сверстан header в состоянии auth

--- src/components/HeaderAuth/HederAuth.js
+++ src/components/HeaderAuth/HederAuth.js
@@ -7,21 +7,22 @@ import account from "../../images/account.svg"
 function HederAuth() {
   return(
     <header className="header__auth">
-      <Link to="/" className="form__logo">
-        <img src={logo} alt="логотип" />
-      </Link>
-      <div className="header__button-box_films">
-        <Link to="/movies" className="header__button-films">
-          Фильмы
+      <div className="header__auth-container"> 
+        <Link to="/" className="header__auth-logo">
+          <img src={logo} alt="логотип" />
         </Link>
-        <Link to="/saved-movies" className="header__button-films">
-          Сохранённые фильмы
-        </Link>
-      </div>
-      <div className="header__button-box">
-        <Link to="/profile" className="header__account-button">
-          <img src={account} alt="аккаунт" />
-        </Link>
+        <div className="header__button-box_films">
+          <Link to="/movies" className="header__button-films">
+            Фильмы
+          </Link>
+          <Link to="/saved-movies" className="header__button-savefilms">
+            Сохранённые фильмы
+          </Link>
+          <Link to="/profile" className="header__account-button">
+            <img src={account} alt="аккаунт" />
+            Аккаунт
+          </Link>
+        </div>
       </div>
     </header>
   );
