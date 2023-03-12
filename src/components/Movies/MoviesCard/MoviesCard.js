4ed6355824c9bf4506d3dafa8a31cda7bec48814
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-13 01:31:13 +0700

    fix MoviesCard

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -20,12 +20,12 @@ function MoviesCard({card}) {
           <h2 className="moviesCard__title">{card.name}</h2>
           {location.pathname === "/saved-movies" &&
               <button type="button" aria-label="удалить фильм" className="moviesCard__button-del" onClick={handleClick}>
-                  <img className="moviesCard__click" alt="удалить" src={deleteFilmButton} />
+                  <img className="moviesCard__delete" alt="удалить" src={deleteFilmButton} />
               </button>}
           {location.pathname === "/movies" &&
               <button type="button" aria-label="сохранить" className={isSaved ? "moviesCard__button-save" : "moviesCard__button"}
                   onClick={handleClick}>
-                  {isSaved ? <img className="moviesCard__click" alt="добавлено" src={saveButton} /> :
+                  {isSaved ? <img className="moviesCard__delete" alt="добавлено" src={saveButton} /> :
                       <img className="moviesCard__add" alt="добавить" src={saveFilmButton} />}
               </button>}
           <p className="moviesCard__duration">{card.time}</p>
