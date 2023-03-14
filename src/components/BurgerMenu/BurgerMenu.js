b987ecacc2d0ffa587962ec24fdc6e85be500e0b
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-14 21:10:01 +0700

    начата верстка burgermenu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -11,19 +11,27 @@ function BurgerMenu() {
 
   return(
     <div className="burgermenu">
-    <button className="hamburger-menu__button" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}></button>
-    {menuIsActive && <div className="hamburger-menu__active">
-      <button className="hamburger-menu__close"></button>
-      <nav className="hamburger-menu__links">
-          <Link className="hamburger-menu__link" to="/">Главная</Link>
-          <Link className="hamburger-menu__link hamburger-menu__link_active" to="/movies">Фильмы</Link>
-          <Link className="hamburger-menu__link" to="/saved-movies">Сохранённые фильмы</Link>
-      </nav>
-      <nav className='hamburger-menu__footer'>
-          <Link className="hamburger-menu__login" to="/profile">Аккаунт</Link>
-          <Link className="hamburger-menu__account" to="/profile" src={account} alt='Логотип аккаунта'></Link>
-      </nav>
-    </div>}
+      
+      {!menuIsActive && <div className="hamburger-menu__notactive"> 
+        <button className="hamburger-menu__button" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}>
+          <img src={burgermenuLogo} alt="бургер меню логотип" />
+        </button>
+      </div>}
+
+      {menuIsActive && <div className="hamburger-menu__active">
+        <button className="hamburger-menu__close" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}>
+          <img src={burgermenuButtonClose} alt="бургер меню закрыть" />
+        </button>
+        <nav className="hamburger-menu__links">
+            <Link className="hamburger-menu__link" to="/">Главная</Link>
+            <Link className="hamburger-menu__link hamburger-menu__link_active" to="/movies">Фильмы</Link>
+            <Link className="hamburger-menu__link" to="/saved-movies">Сохранённые фильмы</Link>
+        </nav>
+        <nav className='hamburger-menu__footer'>
+            <Link className="hamburger-menu__login" to="/profile">Аккаунт</Link>
+            <Link className="hamburger-menu__account" to="/profile" src={account} alt='Логотип аккаунта'></Link>
+        </nav>
+      </div>}
     </div>
   );
 }
