f3aaa6a855a746d6e5cc18419723add201879c03
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-17 01:28:52 +0700

    fix: burgermenu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -12,28 +12,35 @@ function BurgerMenu() {
   return(
     <div className="burgermenu">
       
-      {!menuIsActive && <div className="hamburger-menu__notactive"> 
-        <button className="hamburger-menu__button" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}>
-          <img src={burgermenuLogo} alt="бургер меню логотип" />
-        </button>
-      </div>}
+      {!menuIsActive &&
+        <div className="hamburger-menu__notactive"> 
+          <button className="hamburger-menu__button" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}>
+            <img src={burgermenuLogo} alt="бургер меню логотип" />
+          </button>
+        </div>
+      }
 
-      {menuIsActive && <div className="hamburger-menu__active">
-        <button className="hamburger-menu__close" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}>
-          <img src={burgermenuButtonClose} alt="бургер меню закрыть" />
-        </button>
-        <nav className="hamburger-menu__links">
-            <Link className="hamburger-menu__link" to="/">Главная</Link>
-            <Link className="hamburger-menu__link hamburger-menu__link_active" to="/movies">Фильмы</Link>
-            <Link className="hamburger-menu__link" to="/saved-movies">Сохранённые фильмы</Link>
-        </nav>
-        <nav className='hamburger-menu__footer'>
-          <div className="hamburger-menu__footer-box">
-            <Link className="hamburger-menu__login" to="/profile">Аккаунт</Link>
-            <Link className="hamburger-menu__account" to="/profile" src={account} alt='Логотип аккаунта'></Link>
-          </div>
-        </nav>
-      </div>}
+      {menuIsActive && 
+      <div className="hamburger-bg">
+        <div className="hamburger-menu__active">
+          <button className="hamburger-menu__close" onClick={()=>setMenuIsActive(prev=>setMenuIsActive(!prev))}>
+            <img src={burgermenuButtonClose} alt="бургер меню закрыть" />
+          </button>
+          <nav className="hamburger-menu__links">
+              <Link className="hamburger-menu__link" to="/">Главная</Link>
+              <Link className="hamburger-menu__link hamburger-menu__link_active" to="/movies">Фильмы</Link>
+              <Link className="hamburger-menu__link" to="/saved-movies">Сохранённые фильмы</Link>
+          </nav>
+          <nav className='hamburger-menu__footer'>
+            <div className="hamburger-menu__footer-box">
+              <Link className="hamburger-menu__login" to="/profile">Аккаунт</Link>
+              <Link className="hamburger-menu__account" to="/profile" src={account} alt='Логотип аккаунта'></Link>
+            </div>
+          </nav>
+        </div>
+      </div>
+      
+      }
     </div>
   );
 }
