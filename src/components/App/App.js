131d70a955ea52d0d7d4be1167531b3f06e50d2f
Alexey Filimonov <alexeyf08@gmail.com>
2023-03-21 00:26:44 +0700

    Сделана защита роутов Movies, savaMovies, Profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import Login from '../Login/Login';
 import Register from '../Register/Register';
 import Profile from '../Profile/Profile';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import { ProtectedRoute } from '../ProtectedRoute/ProtectedRoute';
 
 export const CurrentUserContext = createContext();
 const initUser = {name: '', email: ''}
@@ -24,11 +25,33 @@ function App() {
         <div className='App'>
           <Routes>
             <Route exact path="/" element={<Main/>} />
-            <Route exact path="/Movies" element={<Movies searchText={searchText} searchHandler={searchHandler}/>} />
-            <Route exact path="/saved-movies" element={<SavedMovies/>} />
             <Route exact path="/signin" element={<Login/>} />
             <Route exact path="/signup" element={<Register/>} />
-            <Route exact path="/profile" element={<Profile/>} />
+
+            <Route exact path="/profile" 
+                         element={
+                          <ProtectedRoute logedId={logedId}>
+                           <Profile/>
+                          </ProtectedRoute>
+                         }
+            />
+
+            <Route exact path="/Movies" 
+                         element={
+                          <ProtectedRoute logedId={logedId}>
+                            <Movies searchText={searchText} searchHandler={searchHandler}/>
+                          </ProtectedRoute>
+                         }
+            />
+
+            <Route exact path="/saved-movies" 
+                         element={
+                          <ProtectedRoute logedId={logedId}>
+                            <SavedMovies/>
+                          </ProtectedRoute>
+                         }
+            />
+
             <Route exact path="*" element={<Page404/>} />
           </Routes>
         </div>
